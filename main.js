eb805abeedb4fe41a51730c4e3e919fb3b6c5075
yuribertoli <yuribertoli@yahoo.it>
2022-01-31 17:40:40 +0100

    js

--- main.js
+++ main.js
@@ -1,9 +1,67 @@
 const container = document.querySelector(".container");
+const levels = document.getElementById("levels");
+const play = document.getElementsByTagName("button")[0];
 
-for (let i = 1; i <= 100; i++) {
+play.addEventListener("click", function() {
 
-    let square = document.createElement("div");
-    square.classList.add("square");
-    container.appendChild(square);
+    if (levels.value == "Easy") {
 
-}
+        container.innerHTML = "";
+
+        for (let i = 1; i <= 100; i++) {
+    
+            let square = document.createElement("div");
+            square.classList.add("square10");
+            container.appendChild(square);
+    
+            square.addEventListener("click", function() {
+                this.classList.add("active");
+                }
+            )
+        
+        }
+    
+    } else if (levels.value == "Medium") {
+
+        container.innerHTML = "";
+        
+        for (let i = 1; i <= 81; i++) {
+    
+            let square = document.createElement("div");
+            square.classList.add("square9");
+            container.appendChild(square);
+    
+            square.addEventListener("click", function() {
+                this.classList.add("active");
+                }
+            )
+        
+        }
+    
+    } else if (levels.value == "Hard") {
+
+        container.innerHTML = "";
+        
+        for (let i = 1; i <= 49; i++) {
+    
+            let square = document.createElement("div");
+            square.classList.add("square7");
+            container.appendChild(square);
+    
+            square.addEventListener("click", function() {
+                this.classList.add("active");
+                }
+            )
+        
+        }
+    
+    }
+
+})
+
+
+
+
+
+
+
