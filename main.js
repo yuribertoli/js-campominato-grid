0cdfd9d9757dbd122390973d6ba51a5f646c55df
yuribertoli <yuribertoli@yahoo.it>
2022-01-31 17:01:02 +0100

    squares

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-let 
+const container = document.querySelector(".container");
+
+for (let i = 1; i <= 100; i++) {
+
+    let square = document.createElement("div");
+    square.classList.add("square");
+    container.appendChild(square);
+
+}
