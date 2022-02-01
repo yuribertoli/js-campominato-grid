566cd77002f4ba3a8818a24123a10cbd250f131b
yuribertoli <yuribertoli@yahoo.it>
2022-02-01 12:12:00 +0100

    switch

--- main.js
+++ main.js
@@ -2,9 +2,37 @@ const container = document.querySelector(".container");
 const levels = document.getElementById("levels");
 const play = document.getElementsByTagName("button")[0];
 
-function livelloGioco(quadrati, classe) {
+play.addEventListener("click", function() {
 
-    for (let i = 1; i <= quadrati; i++) {
+    container.innerHTML = "";
+
+    const squaresEasy = 100;
+    const squaresMedium = 81;
+    const squaresHard= 49;
+    const classEasy = "square10";
+    const classMedium = "square9";
+    const classHard = "square7";
+
+    switch (levels.value) {
+
+        case "Easy":
+            livelloGioco(squaresEasy, classEasy);
+            break;
+
+        case "Medium":
+            livelloGioco(squaresMedium, classMedium);
+            break;
+
+        case "Hard":
+            livelloGioco(squaresHard, classHard);
+            break;
+    }
+
+})
+
+function livelloGioco(numeroQuadrati, classe) {
+
+    for (let i = 1; i <= numeroQuadrati; i++) {
 
         let square = document.createElement("div");
         square.classList.add(classe);
@@ -20,34 +48,6 @@ function livelloGioco(quadrati, classe) {
     }
 
 }
-
-play.addEventListener("click", function() {
-
-    container.innerHTML = "";
-
-    const squaresEasy = 100;
-    const squaresMedium = 81;
-    const squaresHard= 49;
-    const classEasy = "square10";
-    const classMedium = "square9";
-    const classHard = "square7";
-
-    if (levels.value == "Easy") {
-
-        livelloGioco(squaresEasy, classEasy);
-    
-    } else if (levels.value == "Medium") {
-        
-        livelloGioco(squaresMedium, classMedium);
-
-    } else if (levels.value == "Hard") {
-        
-        livelloGioco(squaresHard, classHard);
-
-    }
-
-})
-
 
 
 
