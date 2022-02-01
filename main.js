a8d50cbb0854a49d43f1f95a65a8a6534e50f5ec
yuribertoli <yuribertoli@yahoo.it>
2022-02-01 11:23:45 +0100

    funzione

--- main.js
+++ main.js
@@ -2,65 +2,48 @@ const container = document.querySelector(".container");
 const levels = document.getElementById("levels");
 const play = document.getElementsByTagName("button")[0];
 
+function livelloGioco(quadrati, classe) {
+
+    for (let i = 1; i <= quadrati; i++) {
+
+        let square = document.createElement("div");
+        square.classList.add(classe);
+        container.appendChild(square);
+
+        square.addEventListener("click", function() {
+            this.classList.add("active");
+            }
+        )
+
+        square.innerHTML = square.innerHTML + i;
+    
+    }
+
+}
+
 play.addEventListener("click", function() {
+
+    container.innerHTML = "";
+
+    const squaresEasy = 100;
+    const squaresMedium = 81;
+    const squaresHard= 49;
+    const classEasy = "square10";
+    const classMedium = "square9";
+    const classHard = "square7";
 
     if (levels.value == "Easy") {
 
-        container.innerHTML = "";
-
-        for (let i = 1; i <= 100; i++) {
-    
-            let square = document.createElement("div");
-            square.classList.add("square10");
-            container.appendChild(square);
-    
-            square.addEventListener("click", function() {
-                this.classList.add("active");
-                }
-            )
-
-            square.innerHTML = square.innerHTML + i;
-        
-        }
+        livelloGioco(squaresEasy, classEasy);
     
     } else if (levels.value == "Medium") {
-
-        container.innerHTML = "";
         
-        for (let i = 1; i <= 81; i++) {
-    
-            let square = document.createElement("div");
-            square.classList.add("square9");
-            container.appendChild(square);
-    
-            square.addEventListener("click", function() {
-                this.classList.add("active");
-                }
-            )
+        livelloGioco(squaresMedium, classMedium);
 
-            square.innerHTML = square.innerHTML + i;
-        
-        }
-    
     } else if (levels.value == "Hard") {
-
-        container.innerHTML = "";
         
-        for (let i = 1; i <= 49; i++) {
-    
-            let square = document.createElement("div");
-            square.classList.add("square7");
-            container.appendChild(square);
-    
-            square.addEventListener("click", function() {
-                this.classList.add("active");
-                }
-            )
+        livelloGioco(squaresHard, classHard);
 
-            square.innerHTML = square.innerHTML + i;
-        
-        }
-    
     }
 
 })
